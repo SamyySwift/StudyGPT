3dffea7b477d357bae810b292060296ea3b89f26
Samuel Okon <samueludofia94@gmail.com>
2023-06-23 09:16:14 +0100

    madee change

--- FlipBot.py
+++ FlipBot.py
@@ -116,6 +116,7 @@ def main():
         if st.button("Use Camera"):
             st.session_state.image_buffer = st.camera_input(label="Capture Image")
         if st.session_state.image_buffer is not None:
+            st.sidebar.write("captured")
             st.write(st.session_state.image_buffer)
             # process_cam_input(image)
 
